03b4e0d92bfee50f5c48780dba8c6607cc173f3e
Anderson Otavio dos Santos <andersonotavio@hotmail.com>
2025-02-08 13:59:38 -0300

    desafio concluído

---
+++ src/DesafioControleFluxo/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package DesafioControleFluxo;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String menssage) {
+        super(menssage);
+    }
+}
